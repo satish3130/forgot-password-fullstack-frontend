e7f753709ce96652383e4c4eb8c110a298864bb4
satish3130 <tsatish086@gmail.com>
2021-12-15 08:39:38 +0530

    error clear

--- src/App.js
+++ src/App.js
@@ -1,11 +1,7 @@
 import React from "react";
 import { BrowserRouter, Redirect, Route, Switch } from "react-router-dom";
 import "./App.css";
-import Changepassword from "./components/changePassword";
-import Emailsent from "./components/emailSent";
-import Forgotpassword from "./components/forgotPassword";
 import Login from "./components/login";
-import Passwordresetsuccessfull from "./components/passwordResetSuccessfull";
 import Register from "./components/register";
 
 function App() {
@@ -15,16 +11,6 @@ function App() {
         <Switch>
           <Route path="/register" component={Register} />
           <Route path="/login" component={Login} />
-          <Route
-            path="/users/forgot-password/:userid/:token"
-            component={Changepassword}
-          />
-          <Route path="/forgotpassword" component={Forgotpassword} />
-          <Route path="/emailsent" component={Emailsent} />
-          <Route
-            path="/password-reset-successfull"
-            component={Passwordresetsuccessfull}
-          />
           <Route exact path="/">
             <Redirect to="/login" />
           </Route>
--- src/components/emailSent.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function Emailsent() {
-  return (
-    <div className="d-flex justify-content-center mt-5">
-      <h1>Link sent Succesfully. Please check your Email</h1>
-    </div>
-  );
-}
